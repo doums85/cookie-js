7d01c6dc1eec76fba0a5cb87257331ac77e021e8
Doums <momoseck8@gmail.com>
2022-09-28 17:23:55 +0200

    update item data

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 export default function Update({ onChangeHandler, dbRequest }) {
   const [found, setFound] = useState(false);
