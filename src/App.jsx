5be3ef23a96b4feef43f0eefa30e89db2cd3929f
Doums <momoseck8@gmail.com>
2022-09-28 15:44:37 +0200

    create a db & collection | add item to collection

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { Add } from './components';
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [dbRequest, setDbRequest] = useState(null);
+
+  useEffect(() => {
+    // 1) Create a DataBase
+    // dbName, dbVersion
+    const dbName = 'manga';
+    const dbVersion = 4;
+    const dbRequest = indexedDB.open(dbName, dbVersion);
+
+    // 2) Handling status db
+    // onupgradeneeded Only if version is changed
+    dbRequest.onupgradeneeded = function (event) {
+      // 3) Create Collection
+      const db = event.target.result;
+
+      // name
+      // {keyPath: 'email'} || {autoIncrement: true or false}
+      /* db.createObjectStore('josei', { autoIncrement: true });
+        db.createObjectStore('kodomo', { autoIncrement: true });
+        db.createObjectStore('shonen', { autoIncrement: true });
+        db.createObjectStore('shojo', { autoIncrement: true });
+        db.createObjectStore('seinen', { autoIncrement: true });
+      */
+    };
+
+    // Success
+    dbRequest.onsuccess = function (event) {
+      setDbRequest(event.target.result);
+    };
+
+    // Error
+    dbRequest.onerror = function (event) {
+      console.log(`Error 🚨 : ${event.error.message}`);
+      setDbRequest(null);
+    };
+  }, []);
+
+  const onChangeHandler = (e) => (setState) => setState(e.target.value);
+
+  return (
+    <div className="App">
+      {/* <Add  onChangeHandler={onChangeHandler} dbRequest={dbRequest} /> 👇 */}
+      <Add {...{ onChangeHandler, dbRequest }} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Add.jsx
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+
+export default function Add( { dbRequest, onChangeHandler }) {
+  const [mangaName, setMangaName] = useState('');
+  const [mangaDescription, setMangaDescription] = useState('');
+  const [mangaAuthor, setMangaAuthor] = useState('');
+
+
+  // Add manga to database
+  function addMangaHandler(e) {
+    e.preventDefault();
+
+    if (!dbRequest) {
+      return alert('There was an error try later...');
+    }
+    // Options: readonly(default) || readwrite || versionchange
+    const transaction = dbRequest.transaction('shonen', 'readwrite');
+    const store = transaction.objectStore('shonen');
+
+    const request = store.add({
+      name: mangaName,
+      author: mangaAuthor,
+      description: mangaDescription,
+    });
+
+    request.onsuccess = function () {
+      alert(`Your manga has been added : ${mangaName}`);
+    };
+
+    request.onerror = function () {
+      alert('Something went wrong');
+    };
+  }
+
+  return (
+    <div>
+      <h1>Create Manga</h1>
+
+      <form>
+        <div>
+          <input
+            onChange={(e) => onChangeHandler(e)(setMangaName)}
+            type="text"
+            placeholder="Manga's name"
+          />
+          <input
+            onChange={(e) => onChangeHandler(e)(setMangaDescription)}
+            type="text"
+            placeholder="Description"
+          />
+          <input
+            onChange={(e) => onChangeHandler(e)(setMangaAuthor)}
+            type="text"
+            placeholder="Who wrote it ?"
+          />
+        </div>
+
+        <input type="submit" onClick={(e) => addMangaHandler(e)} value="Add" />
+      </form>
+    </div>
+  );
+}
